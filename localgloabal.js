81ab3e2282c80adcfdbe5aacd33e426c19131706
Unnati Bansal <bansalunnati279@gmail.com>
2025-07-09 20:03:09 +0530

    changes

---
+++ localgloabal.js
@@ -0,0 +1,12 @@
+const globalVar = "I am global";
+function localScopeDemo(){
+    const localVar = "I am local";
+    console.log(localVar);
+    
+    function innerFunction(){
+        console.log(globalVar);
+    }
+    return innerFunction;
+}
+const myFunc=localScopeDemo();
+myFunc();
